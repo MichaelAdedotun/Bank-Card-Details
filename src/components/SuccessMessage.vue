3612207594cba7644b370ea99a58ccd3393b67c9
Michael Adedotun <adedotun2096@gmail.com>
2025-02-04 17:30:08 +0100

    quick fix

--- src/components/SuccessMessage.vue
+++ src/components/SuccessMessage.vue
@@ -9,7 +9,7 @@ import {useRouter} from "vue-router";
 </script>
 
 <template>
-      <div class="h-screen w-full flex flex-col justify-center items-center">
+      <div class="w-full flex flex-col justify-center items-center mt-16 md:h-screen">
         <img src="/src/assets/icon-complete.svg"
              alt="complete icon"
              class="mb-5"
@@ -18,7 +18,7 @@ import {useRouter} from "vue-router";
         <p class="text-sm font-semibold text-[hsl(279,6%,55%)] tracking-wide mb-8">We've added your card details</p>
         <a href=""
            @click="toCardInput"
-           class="bg-[hsl(278,68%,11%)] w-1/3 py-2 text-center text-[hsl(270,3%,87%)] text-sm rounded-lg">
+           class="bg-[hsl(278,68%,11%)] w-1/2 py-2 text-center text-[hsl(270,3%,87%)] text-sm rounded-lg md:w-1/3">
               Continue
         </a>
       </div>
